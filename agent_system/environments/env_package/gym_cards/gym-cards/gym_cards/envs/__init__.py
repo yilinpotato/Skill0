55c33429997fbbd83bd55eb0c8ba4070a4580e73
leo ma <qqrtqr66@163.com>
2026-05-14 00:57:05 +0800

    删gym

--- agent_system/environments/env_package/gym_cards/gym-cards/gym_cards/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from gym_cards.envs.points import Point24Env
-from gym_cards.envs.ezpoints import EZPointEnv
-from gym_cards.envs.blackjack import BlackjackEnv
-from gym_cards.envs.numberline import NumberLineEnv
--- agent_system/environments/env_package/gym_cards/gym-cards/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="gym-cards",
-    version="0.0.1",
-    packages=['gym_cards'],
-    install_requires=["gymnasium", "numpy", "Pillow"]
-)
